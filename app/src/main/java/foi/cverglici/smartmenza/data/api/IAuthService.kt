916b1ba5ba17fc1669ac3ee06b4868162825598b
ssacaric20 <127140838+ssacaric20@users.noreply.github.com>
2025-11-06 16:29:45 +0100

    Izmjena na IAuthService, posebno dodan AuthResponse data klasa.

--- app/src/main/java/foi/cverglici/smartmenza/data/api/IAuthService.kt
+++ app/src/main/java/foi/cverglici/smartmenza/data/api/IAuthService.kt
@@ -1,17 +1,12 @@
 package foi.cverglici.smartmenza.data.api
 
+import foi.cverglici.smartmenza.data.model.AuthResponse
 import foi.cverglici.smartmenza.data.model.RegistrationRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface AuthService {
+interface IAuthService {
     @POST("api/Korisnici/register") // path s backenda
     suspend fun registerUser(@Body request: RegistrationRequest): Response<AuthResponse>
 }
-
-data class AuthResponse(
-    val userId: Int,
-    val message: String,
-    val jwtToken: String?
-)
