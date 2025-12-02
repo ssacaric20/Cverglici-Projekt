25664dcc9d34aa22df2889b36a21bf29479948b9
ssacaric20 <127140838+ssacaric20@users.noreply.github.com>
2025-12-02 22:28:03 +0100

    Napravljen novi Enum za Menu kategoriju

--- SmartMenza.Data/Models/DailyMenuDto.cs
+++ SmartMenza.Data/Models/DailyMenuDto.cs
@@ -2,9 +2,9 @@
 {
     public class DailyMenuDto
     {
-        // PK
         public int dailyMenuId { get; set; }
         public DateOnly date { get; set; }
+        public int category { get; set; }  // NEW: 1 = Lunch, 2 = Dinner
 
         public ICollection<DailyMenuDishDto> dailyMenuDishes { get; set; } = new List<DailyMenuDishDto>();
     }
