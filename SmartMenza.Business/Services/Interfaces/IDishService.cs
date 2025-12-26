ca173d96412cef2fbd943481a943442e5f6601a3
morejas21 <morejas21@foi.hr>
2025-12-26 23:56:25 +0100

    Dodane metode u IDishService interface za CRUD operacije.

--- SmartMenza.Business/Services/Interfaces/IDishService.cs
+++ SmartMenza.Business/Services/Interfaces/IDishService.cs
@@ -5,5 +5,10 @@ namespace SmartMenza.Business.Services.Interfaces
     public interface IDishService
     {
         Task<DishDetailsResponse?> GetDishDetailsAsync(int id);
+        Task<IEnumerable<DishListResponse>> GetAllDishesAsync();
+        Task<DishDetailsResponse?> CreateDishAsync(CreateDishRequest request);
+        Task<DishDetailsResponse?> UpdateDishAsync(int id, UpdateDishRequest request);
+        Task<bool> DeleteDishAsync(int id);
+
     }
 }
