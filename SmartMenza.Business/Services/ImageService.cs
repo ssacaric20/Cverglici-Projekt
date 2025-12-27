6dfcabbd16a8681efbbd466044a2d1d923d5084c
morejas21 <morejas21@foi.hr>
2025-12-27 16:51:23 +0100

    Kreirana ImageService klasa za obradu slika.

---
+++ SmartMenza.Business/Services/ImageService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+using SmartMenza.Business.Services.Interfaces;
+
+namespace SmartMenza.Business.Services
+{
+    public class ImageService : IImageService
+    {
+        private readonly BlobContainerClient _containerClient;
+
+        public ImageService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var containerName = configuration["AzureStorage:ContainerName"];
+
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            _containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        }
+
+
+    }
+}
