0e4ed60672ea9cb03fe14a5e98d95dd6deed5503
morejas21 <morejas21@foi.hr>
2025-12-27 16:52:23 +0100

    Dodane metode u ImageService za upload i brisanje slika sa Azure Blob-a.

--- SmartMenza.Business/Services/ImageService.cs
+++ SmartMenza.Business/Services/ImageService.cs
@@ -22,7 +22,54 @@ namespace SmartMenza.Business.Services
             var blobServiceClient = new BlobServiceClient(connectionString);
             _containerClient = blobServiceClient.GetBlobContainerClient(containerName);
         }
+        public async Task<string> UploadImageAsync(Stream imageStream, string fileName)
+        {
+            var uniqueFileName = $"{Guid.NewGuid()}_{fileName}";
+            var blobClient = _containerClient.GetBlobClient(uniqueFileName);
 
+            var blobHttpHeaders = new BlobHttpHeaders
+            {
+                ContentType = GetContentType(fileName)
+            };
 
+            await blobClient.UploadAsync(imageStream, new BlobUploadOptions
+            {
+                HttpHeaders = blobHttpHeaders
+            });
+
+            return blobClient.Uri.ToString();
+        }
+
+        public async Task<bool> DeleteImageAsync(string imageUrl)
+        {
+            try
+            {
+
+                var uri = new Uri(imageUrl);
+                var blobName = uri.Segments[^1];
+
+                var blobClient = _containerClient.GetBlobClient(blobName);
+                await blobClient.DeleteIfExistsAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static string GetContentType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName).ToLowerInvariant();
+            return extension switch
+            {
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".png" => "image/png",
+                ".gif" => "image/gif",
+                ".webp" => "image/webp",
+                _ => "application/octet-stream"
+            };
+        }
     }
 }
