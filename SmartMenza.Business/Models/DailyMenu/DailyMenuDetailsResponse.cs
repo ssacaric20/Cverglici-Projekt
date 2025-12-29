517ac8766247029b03825514d34522e44f57429d
morejas21 <morejas21@foi.hr>
2025-12-29 12:34:45 +0100

    Dodani request i response modeli za DailyMenu CRUD.

---
+++ SmartMenza.Business/Models/DailyMenu/DailyMenuDetailsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SmartMenza.Business.Models.DailyMenu
+{
+    public class DailyMenuDetailsResponse
+    {
+        public int DailyMenuId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public List<DailyMenuDishListItemResponse> Dishes { get; set; } = new List<DailyMenuDishListItemResponse>();
+    }
+}
---
+++ SmartMenza.Business/Models/DailyMenu/UpdateDailyMenuRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenza.Business.Models.DailyMenu
+{
+    public class UpdateDailyMenuRequest
+    {
+        [Required(ErrorMessage = "Datum je obavezan")]
+        public string Date { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Kategorija je obavezna")]
+        [Range(1, 2, ErrorMessage = "Kategorija mora biti 1 (Lunch) ili 2 (Dinner)")]
+        public int Category { get; set; }
+
+        [Required(ErrorMessage = "Lista jela je obavezna")]
+        [MinLength(1, ErrorMessage = "Meni mora sadržavati barem jedno jelo")]
+        public List<int> DishIds { get; set; } = new List<int>();
+    }
+}
