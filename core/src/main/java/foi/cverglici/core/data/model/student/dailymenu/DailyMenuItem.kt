4562cf0fb25b0edf8cd175511075ca968cb4fee3
ssacaric20 <127140838+ssacaric20@users.noreply.github.com>
2025-12-29 23:25:13 +0100

    Premještavanje datoteka za dodavanje jela

---
+++ core/src/main/java/foi/cverglici/core/data/model/student/dailymenu/DailyMenuItem.kt
@@ -0,0 +1,11 @@
+package foi.cverglici.core.data.model.student.dailymenu
+
+import com.google.gson.annotations.SerializedName
+
+data class DailyMenuItem(
+    val dailyMenuId: Int,
+    val date: String,
+    val dishId: Int,
+    @SerializedName("jelo")  // backend "jelo", "dish" in code
+    val dish: DishMenuResponse
+)
