ebfadb2efd589391cab401fc749d2b0c265c7f2b
ptonkovic21 <ptonkovic21@student.foi.hr>
2026-01-03 13:41:15 +0100

    API url prebacen na server

--- core/src/main/java/foi/cverglici/core/data/api/student/menu/RetrofitDish.kt
+++ core/src/main/java/foi/cverglici/core/data/api/student/menu/RetrofitDish.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitDish {
-    private const val BASE_URL = "http://10.0.2.2:5166/"
-    //private const val BASE_URL = "https://smartmenza-h5csfahadafnajaq.germanywestcentral-01.azurewebsites.net"
+    //private const val BASE_URL = "http://10.0.2.2:5166/"
+    private const val BASE_URL = "https://smartmenza-h5csfahadafnajaq.germanywestcentral-01.azurewebsites.net"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
