4001d400446536642c6f9fc5e30960627c2bc060
ssacaric20 <127140838+ssacaric20@users.noreply.github.com>
2026-01-30 20:43:32 +0100

    Dodani response klase

---
+++ SmartMenza.Business/Models/Statistics/DishStatisticsResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenza.Business.Models.Statistics
+{
+    public class DishStatisticsResponse
+    {
+        public int DishId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? ImgPath { get; set; }
+        public double? AverageRating { get; set; }
+        public int RatingsCount { get; set; }
+        public int FavoriteCount { get; set; }
+    }
+}
