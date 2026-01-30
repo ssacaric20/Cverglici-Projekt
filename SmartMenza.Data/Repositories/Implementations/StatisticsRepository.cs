0aa02f49a246346975e74b4d6dbb11b3990d2c25
ssacaric20 <127140838+ssacaric20@users.noreply.github.com>
2026-01-30 20:44:17 +0100

    Dodan repozitorij i interface repozitorija za statistiku

---
+++ SmartMenza.Data/Repositories/Implementations/StatisticsRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartMenza.Data.Data;
+using SmartMenza.Data.Entities;
+using SmartMenza.Data.Repositories.Interfaces;
+
+namespace SmartMenza.Data.Repositories.Implementations
+{
+    public sealed class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly AppDBContext _context;
+
+        public StatisticsRepository(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Dish>> GetTopRatedDishesAsync(int count)
+        {
+            return await _context.Dishes
+                .Include(d => d.DishRatings)
+                .Include(d => d.FavoriteDishes)
+                .Where(d => d.DishRatings.Any())
+                .OrderByDescending(d => d.DishRatings.Average(r => (double)r.Rating))
+                .ThenByDescending(d => d.DishRatings.Count)
+                .Take(count)
+                .ToListAsync();
+        }
+
+        public async Task<List<Dish>> GetMostFavoritedDishesAsync(int count)
+        {
+            return await _context.Dishes
+                .Include(d => d.DishRatings)
+                .Include(d => d.FavoriteDishes)
+                .Where(d => d.FavoriteDishes.Any())
+                .OrderByDescending(d => d.FavoriteDishes.Count)
+                .ThenByDescending(d => d.DishRatings.Any()
+                    ? d.DishRatings.Average(r => (double)r.Rating)
+                    : 0)
+                .Take(count)
+                .ToListAsync();
+        }
+
+        public async Task<int> GetFavoriteCountAsync(int dishId)
+        {
+            return await _context.FavoriteDishes
+                .Where(f => f.DishId == dishId)
+                .CountAsync();
+        }
+    }
+}
---
+++ SmartMenza.Data/Repositories/Interfaces/IStatisticsRepository.cs
@@ -0,0 +1,11 @@
+﻿using SmartMenza.Data.Entities;
+
+namespace SmartMenza.Data.Repositories.Interfaces
+{
+    public interface IStatisticsRepository
+    {
+        Task<List<Dish>> GetTopRatedDishesAsync(int count);
+        Task<List<Dish>> GetMostFavoritedDishesAsync(int count);
+        Task<int> GetFavoriteCountAsync(int dishId);
+    }
+}
