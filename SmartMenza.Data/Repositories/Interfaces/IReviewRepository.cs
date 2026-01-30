cde758bde7090dd96fed009c25b714fe16eee5ab
morejas21 <morejas21@foi.hr>
2026-01-30 20:27:12 +0100

    Dodan repository za recenzije.

---
+++ SmartMenza.Data/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenza.Data.Entities;
+
+namespace SmartMenza.Data.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        Task<List<DishRating>> GetReviewsByDishIdAsync(int dishId);
+        Task<DishRating?> GetByIdAsync(int reviewId);
+        Task<DishRating?> GetUserReviewForDishAsync(int userId, int dishId);
+        Task<bool> UserHasReviewedDishAsync(int userId, int dishId);
+        Task AddAsync(DishRating review);
+        void Update(DishRating review);
+        void Remove(DishRating review);
+        Task SaveChangesAsync();
+    }
+}
