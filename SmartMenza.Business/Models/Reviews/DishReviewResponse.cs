46e46313bb5045a506f7e993f5b202f61c4821f1
morejas21 <morejas21@foi.hr>
2026-01-30 20:24:44 +0100

    Dodani modeli za recenzije (Request/Response DTOs).

---
+++ SmartMenza.Business/Models/Reviews/DishReviewResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMenza.Business.Models.Reviews
+{
+    public class DishReviewResponse
+    {
+        public int DishRatingId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+    }
+}
---
+++ SmartMenza.Business/Models/Reviews/UpdateReviewRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenza.Business.Models.Reviews
+{
+    public class UpdateReviewRequest
+    {
+        [Required(ErrorMessage = "Ocjena je obavezna.")]
+        [Range(1, 5, ErrorMessage = "Ocjena mora biti između 1 i 5.")]
+        public int Rating { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Komentar ne smije biti dulji od 500 znakova.")]
+        public string Comment { get; set; } = string.Empty;
+    }
+}
